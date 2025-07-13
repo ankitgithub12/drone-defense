9e484ddc0f18d3788e62a345fbf6371b1740d230
ankitgithub12 <ankit639520@gmail.com>
2025-07-13 16:39:45 +0530

    Force redeploy

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify, request, send_from_directory
+from flask import Flask, render_template, jsonify, send_from_directory
 from flask_cors import CORS
 from detector import DroneDetector
 import threading
